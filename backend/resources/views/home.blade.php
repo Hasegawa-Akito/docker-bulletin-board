4b9899f232bafe15b103963190fc5cbce647e36e
Hasegawa-Akito <h.akito1999@gmail.com>
2021-09-08 23:25:05 +0900

    public/css,js作成 component表示

---
+++ backend/resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <script src="{{asset('js/app.js')}}" defer></script>
+    
+    <title>home</title>
+</head>
+<body>
+    <div id="app">
+    <example-component></example-component>
+    </div>
+</body>
+</html>
